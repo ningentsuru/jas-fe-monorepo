c8459a973b07dab8221cf5666dc33eb8b8de6112
ningentsuru <ningen_tsuru@yahoo.com>
2026-07-27 00:05:01 +0800

    Vercel fix deployment

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,29 +2,29 @@
 import './style.css'
 
 // Atoms
-export { default as AtomToggle } from './atoms/AtomToggle/index'
-export { default as AtomButton } from './atoms/AtomButton/index'
-export { default as AtomSelect } from './atoms/AtomSelect/index'
-export { default as AtomIcon } from './atoms/AtomIcon/index'
-export { default as AtomNavLink } from './atoms/AtomNavLink/index'
-export { default as AtomWordSwap } from './atoms/AtomWordSwap/index'
+export { default as AtomToggle } from './atoms/AtomToggle/index.ts'
+export { default as AtomButton } from './atoms/AtomButton/index.ts'
+export { default as AtomSelect } from './atoms/AtomSelect/index.ts'
+export { default as AtomIcon } from './atoms/AtomIcon/index.ts'
+export { default as AtomNavLink } from './atoms/AtomNavLink/index.ts'
+export { default as AtomWordSwap } from './atoms/AtomWordSwap/index.ts'
 // plop:inject-atoms-component-do-not-removed
 
 // Molecules
-export { default as MoleculeModal } from './molecules/MoleculeModal/index'
-export { default as MoleculeThemeToggle } from './molecules/MoleculeThemeToggle/index'
-export { default as MoleculeNavDropdown } from './molecules/MoleculeNavDropdown/index'
-export { default as MoleculeNavAccordion } from './molecules/MoleculeNavAccordion/index'
-export { default as MoleculeTooltip } from './molecules/MoleculeTooltip/index'
+export { default as MoleculeModal } from './molecules/MoleculeModal/index.ts'
+export { default as MoleculeThemeToggle } from './molecules/MoleculeThemeToggle/index.ts'
+export { default as MoleculeNavDropdown } from './molecules/MoleculeNavDropdown/index.ts'
+export { default as MoleculeNavAccordion } from './molecules/MoleculeNavAccordion/index.ts'
+export { default as MoleculeTooltip } from './molecules/MoleculeTooltip/index.ts'
 // plop:inject-molecules-component-do-not-removed
 
 // Organisms
-export { default as OrganismHeader } from './organisms/OrganismHeader/index'
-export { default as OrganismFooter } from './organisms/OrganismFooter/index'
-export { default as OrganismHero } from './organisms/OrganismHero/index'
-export { default as OrganismNavigation } from './organisms/OrganismNavigation/index'
+export { default as OrganismHeader } from './organisms/OrganismHeader/index.ts'
+export { default as OrganismFooter } from './organisms/OrganismFooter/index.ts'
+export { default as OrganismHero } from './organisms/OrganismHero/index.ts'
+export { default as OrganismNavigation } from './organisms/OrganismNavigation/index.ts'
 // plop:inject-organisms-component-do-not-removed
 
 // Templates
-export { default as TemplateDefaultPortfolio } from './templates/TemplateDefaultPortfolio/index'
+export { default as TemplateDefaultPortfolio } from './templates/TemplateDefaultPortfolio/index.ts'
 // plop:inject-templates-component-do-not-removed
