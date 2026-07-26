aab86991e2d3144e4cabf5733b512b4c469e865e
ningentsuru <ningen_tsuru@yahoo.com>
2026-07-27 00:15:36 +0800

    Vercel fix

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,29 +2,29 @@
 import './style.css'
 
 // Atoms
-export { default as AtomToggle } from './atoms/AtomToggle/AtomToggle.vue'
-export { default as AtomButton } from './atoms/AtomButton/AtomButton.vue'
-export { default as AtomSelect } from './atoms/AtomSelect/AtomSelect.vue'
-export { default as AtomIcon } from './atoms/AtomIcon/AtomIcon.vue'
-export { default as AtomNavLink } from './atoms/AtomNavLink/AtomNavLink.vue'
-export { default as AtomWordSwap } from './atoms/AtomWordSwap/AtomWordSwap.vue'
+export { default as AtomToggle } from './atoms/AtomToggle'
+export { default as AtomButton } from './atoms/AtomButton'
+export { default as AtomSelect } from './atoms/AtomSelect'
+export { default as AtomIcon } from './atoms/AtomIcon'
+export { default as AtomNavLink } from './atoms/AtomNavLink'
+export { default as AtomWordSwap } from './atoms/AtomWordSwap'
 // plop:inject-atoms-component-do-not-removed
 
 // Molecules
-export { default as MoleculeModal } from './molecules/MoleculeModal/MoleculeModal.vue'
-export { default as MoleculeThemeToggle } from './molecules/MoleculeThemeToggle/MoleculeThemeToggle.vue'
-export { default as MoleculeNavDropdown } from './molecules/MoleculeNavDropdown/MoleculeNavDropdown.vue'
-export { default as MoleculeNavAccordion } from './molecules/MoleculeNavAccordion/MoleculeNavAccordion.vue'
-export { default as MoleculeTooltip } from './molecules/MoleculeTooltip/MoleculeTooltip.vue'
+export { default as MoleculeModal } from './molecules/MoleculeModal'
+export { default as MoleculeThemeToggle } from './molecules/MoleculeThemeToggle'
+export { default as MoleculeNavDropdown } from './molecules/MoleculeNavDropdown'
+export { default as MoleculeNavAccordion } from './molecules/MoleculeNavAccordion'
+export { default as MoleculeTooltip } from './molecules/MoleculeTooltip'
 // plop:inject-molecules-component-do-not-removed
 
 // Organisms
-export { default as OrganismHeader } from './organisms/OrganismHeader/OrganismHeader.vue'
-export { default as OrganismFooter } from './organisms/OrganismFooter/OrganismFooter.vue'
-export { default as OrganismHero } from './organisms/OrganismHero/OrganismHero.vue'
-export { default as OrganismNavigation } from './organisms/OrganismNavigation/OrganismNavigation.vue'
+export { default as OrganismHeader } from './organisms/OrganismHeader'
+export { default as OrganismFooter } from './organisms/OrganismFooter'
+export { default as OrganismHero } from './organisms/OrganismHero'
+export { default as OrganismNavigation } from './organisms/OrganismNavigation'
 // plop:inject-organisms-component-do-not-removed
 
 // Templates
-export { default as TemplateDefaultPortfolio } from './templates/TemplateDefaultPortfolio/TemplateDefaultPortfolio.vue'
+export { default as TemplateDefaultPortfolio } from './templates/TemplateDefaultPortfolio'
 // plop:inject-templates-component-do-not-removed
