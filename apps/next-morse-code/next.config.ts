9aee401bc638d4b794c4cdded4a0dc25ff603842
ningentsuru <ningen_tsuru@yahoo.com>
2026-07-31 21:59:02 +0800

    Accept the trades-off of the multi-zones

--- apps/next-morse-code/next.config.ts
+++ apps/next-morse-code/next.config.ts
@@ -2,8 +2,19 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   transpilePackages: ['@repo/ui-react'],
-  basePath: '',
+  basePath: '/multi-zones-morse-code',
   assetPrefix: '/multi-zones-morse-code',
+
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/multi-zones-morse-code',
+        basePath: false,
+        permanent: false,
+      },
+    ]
+  },
 }
 
 export default nextConfig
