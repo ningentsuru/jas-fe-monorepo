b66b89f6ea2e75bca61a3a1989d641eb0c4378d5
ningentsuru <ningen_tsuru@yahoo.com>
2026-07-31 18:54:07 +0800

    Debugging again

--- apps/next-morse-code/next.config.ts
+++ apps/next-morse-code/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   transpilePackages: ['@repo/ui-react'],
+  basePath: '/activity-morse-code',
 }
 
 export default nextConfig
