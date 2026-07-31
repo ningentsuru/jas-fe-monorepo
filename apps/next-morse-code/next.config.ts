1377b355a4ddb654c2ba8641a485ff9111973680
ningentsuru <ningen_tsuru@yahoo.com>
2026-07-31 18:49:18 +0800

    Vercel Multi-Zones debugging

--- apps/next-morse-code/next.config.ts
+++ apps/next-morse-code/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   transpilePackages: ['@repo/ui-react'],
-  assetPrefix: '/activity-morse-code',
 }
 
 export default nextConfig
