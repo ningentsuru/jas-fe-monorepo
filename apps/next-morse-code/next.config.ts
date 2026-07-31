7e44449029af1f4d193f08493bac13e643ea6649
ningentsuru <ningen_tsuru@yahoo.com>
2026-07-31 19:02:57 +0800

    Deploy Multi-Zones

--- apps/next-morse-code/next.config.ts
+++ apps/next-morse-code/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   transpilePackages: ['@repo/ui-react'],
-  basePath: '/activity-morse-code',
+  assetPrefix: 'https://jas-fawn.vercel.app',
 }
 
 export default nextConfig
