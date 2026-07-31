f5c95683a8de8ec1d624408f1c9ce009529df0e8
ningentsuru <ningen_tsuru@yahoo.com>
2026-07-31 19:09:07 +0800

    Is this the fix?

--- apps/next-morse-code/next.config.ts
+++ apps/next-morse-code/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   transpilePackages: ['@repo/ui-react'],
-  assetPrefix: 'https://jas-fawn.vercel.app',
+  basePath: '/activity-morse-code',
+  assetPrefix: '/activity-morse-code',
 }
 
 export default nextConfig
