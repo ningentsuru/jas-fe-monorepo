073ede75b4d3c3e1787aa4be7d40e8a0cfff5e4d
ningentsuru <ningen_tsuru@yahoo.com>
2026-08-06 01:05:55 +0800

    Last error

--- packages/ui-vue/src/molecules/MoleculeTechCard/MoleculeTechCard.spec.ts
+++ packages/ui-vue/src/molecules/MoleculeTechCard/MoleculeTechCard.spec.ts
@@ -1,15 +1,23 @@
 import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 import MoleculeTechCard from './MoleculeTechCard.vue'
-import { Default } from './MoleculeTechCard.stories'
+import meta, { Default } from './MoleculeTechCard.stories'
 
+type MoleculeTechCardProps = InstanceType<typeof MoleculeTechCard>['$props']
+
+const getProps = (storyArgs: typeof Default.args): MoleculeTechCardProps => {
+  return {
+    ...meta.args,
+    ...storyArgs,
+  } as MoleculeTechCardProps
+}
 
 describe('MoleculeTechCard', () => {
   it('renders properly using Storybook args', () => {
     const wrapper = mount(MoleculeTechCard, {
-      props: Default.args,
+      props: getProps(Default.args),
     })
 
-    expect(wrapper.text()).toContain('molecule-tech-card')
+    expect(wrapper.text()).toContain('organism-timeline')
   })
 })
